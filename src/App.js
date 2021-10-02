34feda94ba7535a7c65b94ddae3fb619a36d7a9e
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2021-10-02 18:06:02 -0500

    Update main design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="exteriorCard">
+      <div class="interiorCard">
+        <div class="interiorCircle">
+          <div className="recepientName">Justyna, baby</div>
+          <div className="recepientQuestion">Fine Arts?</div>
+          <div>
+            <Button className="buttonYes">Yes</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
